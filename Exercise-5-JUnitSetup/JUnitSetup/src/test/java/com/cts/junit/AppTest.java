478bf548af5eddf44e5a98314b5bb5aa77b588d4
Nandhini J <230290.cs@rmkec.ac.in>
2026-07-08 14:29:35 +0530

    Completed Exercise 5 - Setting Up JUnit

---
+++ Exercise-5-JUnitSetup/JUnitSetup/src/test/java/com/cts/junit/AppTest.java
@@ -0,0 +1,12 @@
+package com.cts.junit;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void testSample() {
+        assertTrue(true);
+    }
+}
